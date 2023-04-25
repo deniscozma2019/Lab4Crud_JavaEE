b5943ca7e0f6a66e5f7465d0bf641f8a545ca110
deniscozma2019 <denis.cozma2015@gmail.com>
2023-04-25 13:21:26 +0300

    Initial commit

---
+++ src/main/java/com/example/lab4crud_javaee/Lab4CrudJavaEeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab4crud_javaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4CrudJavaEeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4CrudJavaEeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4crud_javaee/Lab4CrudJavaEeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4crud_javaee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4CrudJavaEeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
